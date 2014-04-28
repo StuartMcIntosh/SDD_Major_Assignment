1303cb5199475cfb72d9a17aa8d50b6c85944226
Stuart McIntosh <stuartm37@gmail.com>
2014-04-28 21:25:23 +1000

    28/04/2014 21:24
    working on questions file
    fixed up spacing, other things
    still not able to read questions file into program

--- sdd major-1.py
+++ sdd major-1.py
@@ -3,10 +3,10 @@
 ## Due Date: 09/05/2014
 
 import pygame, random, time, array
+
 pygame.init()
 
-
-#The variable running the Game while Loop
+#Variable that runs the main Game Loop
 Game = True
 
 #-Screen
@@ -33,16 +33,34 @@ screenY = numTileHigh * tileSize
 screen = pygame.display.set_mode((screenX, screenY))
 pygame.display.set_caption("Stuart McIntosh-Yr 12 SDD Major Project")
 
+#-Random Number Generation
+ranNum = str(random.randint(0, 9))
 
 #Manages the text to display on screen, including colour and font
-textToDisplay = "Hello"
+textToDisplay = ranNum
 font = pygame.font.Font(None, 40)
 onScreenText = font.render(textToDisplay, True, black)
 onScreenText_display = onScreenText.get_rect(centerx=screenX/2, centery=screenY/2)
 
-#-Random Number Generation
-##ranNum = random.randint(0, 9)
-##number = str(ranNum)
+#Movement
+s = 'stop'
+u = 'up'
+r = 'right'
+d = 'down'
+l = 'left'
+movedir = s
+
+
+Questions Text File
+qtf = open('questions.txt')
+questions = array.array('c')
+qs = str(qtf)
+l = len(qs)
+questions.fromfile(qtf, 375)
+ql = list(qtf)
+
+
+
 
 #-Game Loop
 while Game == True:
@@ -55,33 +73,42 @@ while Game == True:
             #Escape Key
             if event.key == pygame.K_ESCAPE:
                 Game = False
-                
-        #Character Movement
-            #Move Up-Up Arrow
+    #Arrow Key Movement
+            #Up Key
             if event.key == pygame.K_UP:
-                if car.rect.top > 0:
-                    car.rect.top -= tileSize
-                    
-            #Move Down-Down Arrow
-            if event.key == pygame.K_DOWN:
-                if car.rect.bottom < screenX:                 
-                    car.rect.bottom += tileSize
-                    
-            #Move Right-Right Arrow
+                movedir = u
+            #Right Key
             if event.key == pygame.K_RIGHT:
-                if car.rect.right < screenY:
-                    car.rect.right += tileSize
-                    
-            #Move Left-Left Arrow
+                movedir = r
+            #Down Key
+            if event.key == pygame.K_DOWN:
+                movedir = d
+            #Left Key
             if event.key == pygame.K_LEFT:
-                 if car.rect.left> 0:
-                    car.rect.left -= tileSize
+                movedir = l
+        else:
+            movedir = s
+
+    if movedir == u:
+        if car.rect.top > 0:
+            car.rect.top -= 1
+            time.sleep(0.005)
+    if movedir == r:
+         if car.rect.right < screenY:
+             car.rect.right += 1
+             time.sleep(0.005)
+    if movedir == d:
+        if car.rect.bottom < screenX:
+            car.rect.bottom += 1
+            time.sleep(0.005)
+    if movedir == l:
+          if car.rect.left > 0:
+             car.rect.left -= 1
+             time.sleep(0.005)
 
     screen.fill(white)
     car_group.draw(screen)
     screen.blit(onScreenText, onScreenText_display)
     pygame.display.update()
-    
 pygame.quit()
-
 
